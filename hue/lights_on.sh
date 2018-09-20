56fac81b9f24f2aab19a10b5d45c289f802bbc36
p.budrewicz <p.budrewicz@samsung.local>
2018-09-20 10:57:55 +0200

    turn on the lights... as they were

---
+++ hue/lights_on.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### author: p.budrewicz
+
+if [ "$1" = "" ] ; then
+  echo "$0 light_list"
+  exit 0
+fi
+
+
+. $( dirname $0 )/user.key 
+
+
+for light do
+   curl -s -X PUT -d '{"on":true}' http://$bridge_ip/api/$user_key/lights/$light/state 
+done
