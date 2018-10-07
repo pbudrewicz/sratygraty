353e0acf9ed35f265c4780624a096d8c1db554ba
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2018-10-07 08:43:41 +0200

    improved no/bad command reaction

--- hue/strobe.sh
+++
@@ -1,9 +0,0 @@
-
-. user.key
-
-for light in 1 2 3 ; do
-        curl -X PUT -d '{"1":"040000FFFF00003333000033330000FFFFFFFFFF"}' http://$bridge_ip/api/$user_key/lights/$light/pointsymbol  # blue fast
-done
-
-curl -X PUT -d '{"symbolselection":"01010C010101020103010401050106010701080109010A010B010C","duration":4000}' http://$bridge_ip/api/$user_key/groups/3/transmitsymbol
-
