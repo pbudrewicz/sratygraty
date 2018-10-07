94259628d2294c9f08728f999caa906fc735544b
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2018-10-07 20:54:43 +0200

    filtering to today

--- weather/weather_fun.sh
+++ weather/weather_fun.sh
@@ -22,11 +22,22 @@ C_from_K () {
 
 CAST_COUNT=$( weather count '{list}' )
 
+
+is_today () {
+  TODAY=$( date +%Y-%m-%d )
+  if [ "$1" = "$TODAY" ] ; then
+	  return 1
+  else
+	  return 0
+  fi
+}
+
 for i in $(seq 0 $(( $CAST_COUNT - 1 )) ) ; do
     temperature=$( weather get "{list}[$i]{main}{temp}" )
     T=$( C_from_K $temperature )
     condition=$( weather get "{list}[$i]{weather}[0]{main}" ) 
     date=$( weather get "{list}[$i]{dt_txt}" )
+    if is_today $date ; then break ; fi 
     COLOR=$( $DIR/../hue/temp2color.sh $T)
     echo $date $condition $T color:$COLOR
     $DIR/../hue/hue -l 3 set color xy $COLOR 200
