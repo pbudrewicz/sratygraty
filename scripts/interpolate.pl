21227ad767927ef24816416dc8f8297ae8beba5a
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2018-10-09 23:23:27 +0200

    now with parametrized output format

--- scripts/interpolate.pl
+++ scripts/interpolate.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 $steps = ($ARGV[0] eq "") ? 10 : $ARGV[0];
+shift @ARGV;
 $line = <STDIN>;
 chomp $line;
 @prevflds = split ' ', $line;
@@ -9,7 +10,15 @@ while (<STDIN>) {
     @flds = split ' ';
     for ($i=1; $i<$steps; $i++ ) {
 	for ($f=0; $f < scalar(@flds); $f++) {
-	    print STDOUT ($flds[$f] - $prevflds[$f])/$steps * $i + $prevflds[$f], " ";
+		$fmt = $ARGV[$f];
+		if ($fmt =~ /^\*/) { 
+			$fmt =~ s/\*//; 
+                        printf STDOUT $fmt, ((($flds[$f] / $prevflds[$f])**(1.0/$steps)) ** $i) * $prevflds[$f];
+		} else {
+		       	$fmt = "%f" if $fmt eq "";
+                        printf STDOUT $fmt, ($flds[$f] - $prevflds[$f])/$steps * $i + $prevflds[$f];
+		}
+	        print STDOUT " ";
 	}
 	print STDOUT "\n";
 	
