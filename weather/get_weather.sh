248380869ad9803098164fee42f2a728cf9f61d5
p.budrewicz <p.budrewicz@samsung.local>
2018-11-05 16:59:25 +0100

    id and location support

--- weather/get_weather.sh
+++ weather/get_weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-TEMP=$( getopt -o fhl:sw --long forecast,help,location:,silent,weather -n $0 -- "$@" )
+TEMP=$( getopt -o fhi:l:sw --long forecast,help,id,location:,silent,weather -n $0 -- "$@" )
 
 if [ $? != 0 ] ; then echo "Cannot parse options. Terminating..." >&2 ; exit 1 ; fi
 
@@ -13,6 +13,7 @@ Usage:
     Options are
       -f|--forecast - show forecast
       -h|--help     - show this help
+      -i|--id       - show data for city with this id
       -l|--location - show data for this location
       -s|--silent   - run silently
       -w|--weather  - show current weather
@@ -23,15 +24,20 @@ Usage:
 LOCATION="Warsaw,PL"
 DATA_TYPE="weather"
 SILENT=""
+ID=756135
 
 while true ; do
     case "$1" in
         -f|--forecast)
             DATA_TYPE=forecast; shift;;
         -l|--location)
-            LOCATION="$2"; shift 2;;
+            LOCATION="$2"; shift 2
+            MODE=location;;
         -h|--help)
             show_help 0;;
+        -i|--id)
+	    ID="$2"; shift 2
+            MODE=id;;
         -s|--silent)
             SILENT="-s"; shift;;
         -w|--weather)
@@ -46,5 +52,11 @@ done
 
 . $( dirname $0 )/weather.key
 
-curl ${SILENT} -X GET "api.openweathermap.org/data/2.5/${DATA_TYPE}?q=${LOCATION}&mode=json&APPID=$weather_key"
+if [ "$MODE" = "location" ] ; then
+  curl ${SILENT} -X GET "api.openweathermap.org/data/2.5/${DATA_TYPE}?q=${LOCATION}&mode=json&APPID=$weather_key"
+elif [ "$MODE" = "id" ] ; then
+  curl ${SILENT} -X GET "api.openweathermap.org/data/2.5/${DATA_TYPE}?id=${ID}&mode=json&APPID=$weather_key"
+else
+  curl ${SILENT} -X GET "api.openweathermap.org/data/2.5/${DATA_TYPE}?lat=50.333673&lon=22.972408&mode=json&APPID=$weather_key"
+fi
 
