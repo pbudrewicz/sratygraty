42aa94902814b56c45e0199694dc27a76d4fa801
p.budrewicz <p.budrewicz@samsung.local>
2018-12-06 17:33:47 +0100

    advent

---
+++ advent/2018/5/day201805.hs
@@ -0,0 +1,26 @@
+import Data.Char
+
+reduce :: String -> String
+reduce []                                                 = []
+reduce (x:[])                                             = [x]
+reduce (x:y:rest) | isLower(y) && x == toUpper(y)         = reduce rest
+                  | isUpper(y) && x == toLower(y)         = reduce rest
+                  | otherwise                             = x:reduce (y:rest)
+
+reduct :: String -> String
+reduct [] = []
+reduct x | x == reduce x                = x 
+         | otherwise                    = reduct (reduce x)
+
+
+testunit :: Char -> String -> String
+testunit x poly = reduct [ y | y <- poly, toUpper(y) /= toUpper(x) ]
+
+findbest :: String -> [Int]
+findbest poly = minimum [ length ( testunit u poly ) | u <- ['a'..'z'] ]
+
+
+main = do
+polymer <- getLine
+putStrLn ( show (findbest polymer ) )
+
