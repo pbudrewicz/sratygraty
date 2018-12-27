414351b32131a3121301d9df926c35dbb1189ef1
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2018-12-27 22:32:01 +0100

    before data structure change

--- advent/2018/8/day201808.hs
+++ advent/2018/8/day201808.hs
@@ -1,4 +1,4 @@
-data Tree = Node { children :: [Tree], meta :: [Int] } deriving Show
+data Tree = Node { children :: [Tree], meta :: [Int], len :: Int } | Empty | BadData deriving Show
 data TreeData = TreeData { tree :: Tree, len :: Int } deriving Show
 -- type Meta = [Int]
 
@@ -16,20 +16,26 @@ data TreeData = TreeData { tree :: Tree, len :: Int } deriving Show
 -- getMeta Node( _, m ) = m
 
 makeTree :: [Int] -> TreeData
-makeTree [] = TreeData { tree=Node { children=[], meta=[] }, len=0 }
-makeTree (0:y:xs) = TreeData { tree=Node { children=[], meta=(take y xs)}, len=y } 
-makeTree (x:y:xs) = TreeData { tree=Node { children=(tree firstChild):(children (tree subTree)), meta=[] }, len=(len subTree) } where 
+makeTree [] = TreeData { tree=Empty, len=0 }
+makeTree (0:y:xs) = TreeData { tree=Node { children=[], meta=(take y xs)}, len=y+2 }
+makeTree (x:_:[]) = TreeData { tree=BadData, len=2 }
+makeTree (x:y:xs) = TreeData { tree=Node { children=(tree firstChild):(children (tree subTree)), meta=(take y (drop length xs)) }, len=length } where 
                                                                                                                        firstChild = makeTree xs
                                                                                                                        subTree = makeTree( x-1:y:(drop (len firstChild) xs ))
+                                                                                                                       length = (len firstChild) + (len subTree)
+
+-- makeSubTree :: [Int] -> [TreeData]
 
 -- sumMeta :: [Int] -> Int
 -- sumMeta [] = 0
 -- sumMeta (0:y:xs) = sum (take y xs) 
 
 sumMeta :: Tree -> Int
-sumMeta Node { children = []} = sum meta
-sumMeta Node {  } = sum meta + sum (map sumMeta children )
+sumMeta Empty = 0
+sumMeta Node { children = [], meta=meta } = sum meta
+sumMeta Node { children = children, meta = meta } = sum meta + sum (map sumMeta children )
 
 main = do
   input <- getLine
-  putStrLn show (makeTree (map read (words input)))
+  putStrLn (show  (makeTree (map read (words input))))
+  putStrLn (show (sumMeta (tree (makeTree (map read (words input))))))
