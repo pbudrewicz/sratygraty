1230bde4e422c4dc35b9e3dbec90311538f7ba79
p.budrewicz <p.budrewicz@samsung.local>
2018-12-28 16:07:40 +0100

    second part list (probably) solution

--- advent/2018/1/day201801.hs
+++ advent/2018/1/day201801.hs
@@ -1,9 +1,21 @@
+import qualified Data.Set as Set
+
 myread :: String -> Int
 myread ('-':num) = -1 * read num 
 myread ('+':num) = read num
 
 
+keepRepeating :: [Int] -> [Int]
+keepRepeating x = x ++ keepRepeating x
+
+firstDouble :: [Int] -> [Int] -> Int
+firstDouble seen (x:xs) | elem x seen = x
+                        | otherwise = firstDouble (x:seen) xs
+
+freqList :: String -> [Int]
+freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating (map myread (words input))) (freqList input) ]
+
 main = do
   input <- getContents
-  putStrLn (show (map myread (words input)  ))
   putStrLn (show (foldl (\a n -> a + n ) 0 (map myread (words input))))
+  putStrLn (show (firstDouble [] (freqList input)))
