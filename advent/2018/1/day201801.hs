f288f09975f78646c4ca7dcc6fc286caaf373d2a
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-02 22:36:54 +0100

    second part solution without fancy recursion like fibo

--- advent/2018/1/day201801.hs
+++ advent/2018/1/day201801.hs
@@ -4,22 +4,18 @@ myread :: String -> Int
 myread ('-':num) = -1 * read num 
 myread ('+':num) = read num
 
-
-keepRepeating :: Int -> [Int] -> [Int]
-keepRepeating 0 x = x ++ keepRepeating 0 x
-keepRepeating 1 x = x 
-keepRepeating n x = x ++ keepRepeating (n-1) x
-
 firstDouble :: Set.Set Int -> [Int] -> [Int]
 firstDouble seen [] = []
 firstDouble seen (x:xs) | Set.member x seen = [x]
                         | otherwise = firstDouble (Set.insert x seen) xs
 
-freqList :: [Int] -> [Int]
-freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating 0 input) (freqList input) ]
+freqList :: Int -> [Int] -> [Int]
+freqList n [] = n:[] -- should not happen
+freqList 0 (x:[]) = x:[] -- should not happen
+freqList n (x:xs) = (n+x):(freqList (n+x) xs)
 
 main = do
   input <- getContents
   let myinput = (map myread (words input)) in 
 --    putStrLn (show (foldl (\a n -> a + n ) 0 myinput ))
-    putStrLn (show (firstDouble Set.empty (freqList myinput)))
+    putStrLn (show (firstDouble Set.empty (freqList 0 (cycle myinput))))
