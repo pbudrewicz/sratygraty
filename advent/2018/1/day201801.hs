8bb6244a43a48e0f344ea704990d2c679e1e9780
p.budrewicz <p.budrewicz@samsung.local>
2019-01-02 17:06:27 +0100

    some optimizations...

--- advent/2018/1/day201801.hs
+++ advent/2018/1/day201801.hs
@@ -1,21 +1,25 @@
 import qualified Data.Set as Set
 
-myread :: String -> Integer
+myread :: String -> Int
 myread ('-':num) = -1 * read num 
 myread ('+':num) = read num
 
 
-keepRepeating :: [Integer] -> [Integer]
-keepRepeating x = x ++ keepRepeating x
+keepRepeating :: Int -> [Int] -> [Int]
+keepRepeating 0 x = x ++ keepRepeating 0 x
+keepRepeating 1 x = x 
+keepRepeating n x = x ++ keepRepeating (n-1) x
 
-firstDouble :: Set.Set Integer -> [Integer] -> [Integer]
-firstDouble seen (x:xs) | Set.member x seen = x:(Set.toList seen)
+firstDouble :: Set.Set Int -> [Int] -> [Int]
+firstDouble seen [] = []
+firstDouble seen (x:xs) | Set.member x seen = [x]
                         | otherwise = firstDouble (Set.insert x seen) xs
 
-freqList :: String -> [Integer]
-freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating (map myread (words input))) (freqList input) ]
+freqList :: [Int] -> [Int]
+freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating 0 input) (freqList input) ]
 
 main = do
   input <- getContents
-  putStrLn (show (foldl (\a n -> a + n ) 0 (map myread (words input))))
-  putStrLn (show (firstDouble Set.empty (freqList input)))
+  let myinput = (map myread (words input)) in 
+--    putStrLn (show (foldl (\a n -> a + n ) 0 myinput ))
+    putStrLn (show (firstDouble Set.empty (freqList myinput)))
