0d9714bc7cec6a4d675caad88751561b3a4601f2
p.budrewicz <p.budrewicz@samsung.local>
2019-01-02 17:07:00 +0100

    some unnecessary "fun" with data

--- advent/2018/1/day201801_list.hs
+++ advent/2018/1/day201801_list.hs
@@ -1,23 +1,26 @@
 import qualified Data.Set as Set
 
-myread :: String -> Integer
+myread :: String -> Int
 myread ('-':num) = -1 * read num 
 myread ('+':num) = read num
 
 
-keepRepeating :: [Integer] -> [Integer]
-keepRepeating x = x ++ keepRepeating x
+keepRepeating :: Int -> [Int] -> [Int]
+keepRepeating 0 x = x ++ keepRepeating 0 x
+keepRepeating 1 x = x
+keepRepeating n x = x ++ keepRepeating (n-1) x
 
-firstDouble :: [Integer] -> [Integer] -> [Integer]
-firstDouble seen (x:xs) | elem x seen = (x:seen)
+firstDouble :: [Int] -> [Int] -> [Int]
+firstDouble seen [] = []
+firstDouble seen (x:xs) | elem x seen = x
                         | otherwise = firstDouble (x:seen) xs
 
-freqList :: String -> [Integer]
--- freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating (map myread (words input))) (freqList input) ]
-freqList input =  0 : [ x + a | (a, x) <-  zip (map myread (words input)) (freqList input) ]
+freqList :: String -> [Int]
+freqList input =  0 : [ x + a | (a, x) <-  zip (keepRepeating 10 (map myread (words input))) (freqList input) ]
+-- freqList input =  0 : [ x + a | (a, x) <-  zip (map myread (words input)) (freqList input) ]
 
 main = do
   input <- getContents
-  putStrLn (show (foldl (\a n -> a + n ) 0 (map myread (words input))))
-  putStrLn (show ( freqList input))
-  -- putStrLn (show (firstDouble [] (freqList input)))
+  -- putStrLn (show (foldl (\a n -> a + n ) 0 (map myread (words input))))
+  -- let a=(freqList input) in putStrLn (show ( [minimum a, maximum a, length a, 0, 0, 0] ++ (firstDouble [] a ))) 
+  putStrLn (show (firstDouble [] (freqList input)))
