6df9a254386a3c1938eb8079ea4012f49195ec16
p.budrewicz <p.budrewicz@samsung.local>
2019-01-03 16:55:08 +0100

    recurrent structure instead of recurrent function

--- advent/2018/01/day201801.hs
+++ advent/2018/01/day201801.hs
@@ -9,13 +9,15 @@ firstDouble seen [] = []
 firstDouble seen (x:xs) | Set.member x seen = [x]
                         | otherwise = firstDouble (Set.insert x seen) xs
 
-freqList :: Int -> [Int] -> [Int]
-freqList n [] = n:[] -- should not happen
-freqList 0 (x:[]) = x:[] -- should not happen
-freqList n (x:xs) = (n+x):(freqList (n+x) xs)
+-- freqList :: Int -> [Int] -> [Int]
+-- freqList n [] = n:[] -- should not happen
+-- freqList 0 (x:[]) = x:[] -- should not happen
+-- freqList n (x:xs) = (n+x):(freqList (n+x) xs)
 
+freqList :: [Int] -> [Int]
+freqList input = output where output =  0 : [ x + a | (a, x) <-  zip (cycle input) output ]
 main = do
   input <- getContents
   let myinput = (map myread (words input)) in 
 --    putStrLn (show (foldl (\a n -> a + n ) 0 myinput ))
-    putStrLn (show (firstDouble Set.empty (freqList 0 (cycle myinput))))
+    putStrLn (show (firstDouble Set.empty (freqList (cycle myinput))))
