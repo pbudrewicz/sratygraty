700813e8afa5308fddb5148020192927cf05a440
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-07 23:33:06 +0100

    part one solution [201804]

--- advent/2018/04/day201804.hs
+++ advent/2018/04/day201804.hs
@@ -7,6 +7,7 @@ type Minute = Int
 data State = Sleep|Awake|Id Int deriving (Read, Show, Eq)
 type LogRec = (Minute, State)
 type SleepTime = (Id, Int)
+type SleepPeriod = (Int,Int)
 
 textToRec :: String -> LogRec
 textToRec x | x =~ patGuard :: Bool = let (b,m,a,[g1,g2]) = (x =~ patGuard :: (String, String, String, [String]))
@@ -46,16 +47,37 @@ longestSleeper (x@(i,s):xs) | s > ls = x
                             | otherwise   = l
                               where l@(li,ls) = longestSleeper xs
 
-mostMinute :: Id -> [LogRec] -> Int
-mostMinut guard alog = maximum [ (m,countMinutes guard alog m) | m <- [0..59] ]
 
-countMinutes Id -> [LogRec] -> Int -> Int
-countMinutes guard alog min = 
+
+mostMinute :: [(Int,Int)] -> (Int,Int)
+mostMinute [] = (-1,0)
+mostMinute (x:[]) = x
+mostMinute (x@(m,c):xs) | c > mc = x
+                        | otherwise = mM
+                          where mM@(mm,mc) = mostMinute xs
+
+countMinutes :: Id -> [LogRec] -> Int -> Int
+countMinutes guard alog min = length [ (f,t) | (f,t) <- sleepPeriods guard alog, min >= f && min < t ]
+
+sleepPeriods :: Id -> [LogRec] -> [SleepPeriod]
+sleepPeriods guard alog = (\(id,acc,x) -> acc) (foldl (\(i, acc, t) (m, s) -> case (i, s) of
+                                                                                            (id,Awake) -> if id == guard then (i, (t,m):acc, m)
+                                                                                                                         else (i, acc, m)
+                                                                                            (_,Id g)   -> (g, acc, m)
+                                                                                            _          -> (i, acc, m)
+                                                                                                    
+                                                              ) (-1, [], 0) alog)
+
+showPeriod :: Id -> SleepPeriod -> String
+showPeriod guard (from, to) = "Guard " ++ show guard ++ " slept from " ++ show from ++ " to " ++ show to
 
 main = do
   input <- getContents
   let myinput = (lines input) 
-      alog = map textToRec (sort myinput) in
+      alog = map textToRec (sort myinput)
+      guard = (\(g,s) -> g) (longestSleeper [  getSleepTime grd alog | grd <- getGuards alog ])
+      minute = (\(m,c) -> m) (mostMinute [(m,countMinutes guard alog m) | m <- [0..59]]) in
 --      putStrLn (unlines (map showSleeper [  getSleepTime guard alog | guard <- getGuards alog ]))
-      putStrLn (show (longestSleeper [  getSleepTime guard alog | guard <- getGuards alog ]))
+      putStrLn (show (minute * guard))
+--      putStrLn (show (longestSleeper [  getSleepTime guard alog | guard <- getGuards alog ]))
 --      putStrLn (unlines (map show alog))
