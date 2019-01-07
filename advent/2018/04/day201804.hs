382979269d2da563ee9e2b581e4820186a16dccc
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-07 22:33:57 +0100

    work in progress solution day 201804

--- advent/2018/04/day201804.hs
+++ advent/2018/04/day201804.hs
@@ -4,7 +4,7 @@ import Text.Regex.Posix -- for regex
 type Id = Int 
 type Minute = Int
 
-data State = Sleep|Awake|Id Int deriving (Read, Show)
+data State = Sleep|Awake|Id Int deriving (Read, Show, Eq)
 type LogRec = (Minute, State)
 type SleepTime = (Id, Int)
 
@@ -19,21 +19,43 @@ textToRec x | x =~ patGuard :: Bool = let (b,m,a,[g1,g2]) = (x =~ patGuard :: (S
                   patFalls = ":([0-9]{2}).*falls"
                   patGuard = ":([0-9]{2}).*Guard #([0-9]+) "
 
-getGuards :: [LogRec] -> [Int]
+getGuards :: [LogRec] -> [Id]
 getGuards []  = []
 getGuards (l@(m,Id g):ls) | g `elem` glist = glist
                           | otherwise = g:glist
                           where glist = getGuards ls
 getGuards (l:ls) = getGuards ls
+                                
 
-getSleep :: Id -> [LogRec] -> SleepTime
-getSleep g alog = scanl (\id (m, s) -> case s of 
-                                              (m,Id 
-                         
+getSleepTime :: Id -> [LogRec] -> SleepTime
+getSleepTime guard alog = (\(id,slp,x) -> (guard,slp)) (foldl (\(i, acc, t) (m, s) -> case (i, s) of
+                                                                                            (id,Awake) -> if id == guard then (i, acc+m-t, m)
+                                                                                                                         else (i, acc, m)
+                                                                                            (_,Id g)   -> (g, acc, m)
+                                                                                            _          -> (i, acc, m)
+                                                                                                    
+                                                              ) (-1, 0, 0) alog)
+
+showSleeper :: SleepTime -> String
+showSleeper (id,t) = "Guard " ++ show id ++ " sleeped " ++ show t
+
+longestSleeper :: [SleepTime] -> SleepTime
+longestSleeper [] = (-1,0)
+longestSleeper (x:[]) = x
+longestSleeper (x@(i,s):xs) | s > ls = x
+                            | otherwise   = l
+                              where l@(li,ls) = longestSleeper xs
+
+mostMinute :: Id -> [LogRec] -> Int
+mostMinut guard alog = maximum [ (m,countMinutes guard alog m) | m <- [0..59] ]
+
+countMinutes Id -> [LogRec] -> Int -> Int
+countMinutes guard alog min = 
 
 main = do
   input <- getContents
   let myinput = (lines input) 
       alog = map textToRec (sort myinput) in
-      putStrLn (show (getGuards alog))
+--      putStrLn (unlines (map showSleeper [  getSleepTime guard alog | guard <- getGuards alog ]))
+      putStrLn (show (longestSleeper [  getSleepTime guard alog | guard <- getGuards alog ]))
 --      putStrLn (unlines (map show alog))
