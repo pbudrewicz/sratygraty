901c672a737f54a1ccb6b8dbaf65f988a41004ca
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-13 17:42:34 +0100

    WIP first part [201810]

---
+++ advent/2018/10/day201810.hs
@@ -0,0 +1,39 @@
+import Text.Regex.Posix
+
+type Coord = (Int, Int)
+type Velo  = (Int, Int)
+
+readConstraints :: [String] -> [(Coord,Velo)]
+readConstraints [] = []
+readConstraints (s:ss) = let (_,_,_,[x,y,vx,vy]) = (s =~ "position=< *(-?[0-9]+), *(-?[0-9]+)> velocity=< *(-?[0-9]+), *(-?[0-9]+)>" :: (String, String, String, [String]))
+                         in ((read x,read y),(read vx, read vy)):(readConstraints ss)
+
+
+
+getBounds :: [(Coord,Velo)] -> (Int,Int,Int,Int)
+getBounds points = foldl getBounds' (x,x,y,y) points
+  where ((x,y), _ ) = head points
+
+getBounds' :: (Int, Int, Int, Int) -> ((Int,Int),(Int, Int)) -> (Int, Int, Int, Int) 
+getBounds' (n,s,w,e) ((x,y),_) = (mn, ms, mw, me)
+                              where mn = min n y
+                                    ms = max s y
+                                    mw = min w x
+                                    me = max e x
+
+
+showMessage :: [(Coord,Velo)] -> [String]
+showMessage points = let  coords = map (\(c,v) -> c) points
+                          ((x0,y0), _ ) = head points
+                          bounds@(n,s,w,e) = foldl getBounds' (x0,x0,y0,y0) points
+                     in [ [  if (x,y) `elem` coords then '#' else '.' |   x <- [w..e] ] | y <- [n..s] ]
+
+nextMove :: [(Coord,Velo)] -> [(Coord, Velo)]
+nextMove points = map (\((x,y),(vx,vy)) -> ((x+vx,y+vy),(vx,vy))) points
+
+main = do
+  input <- getContents
+  let constr = readConstraints (lines input)
+      ((x,y),_) = head constr  in
+    putStrLn (show (getBounds constr))
+  
