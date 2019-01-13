fbdd942c3cb6c37d73581e82fc04335f9667acba
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-13 22:25:00 +0100

    part one solution [201810] there will be no part two solution - it was done outside

--- advent/2018/10/day201810.hs
+++ advent/2018/10/day201810.hs
@@ -26,7 +26,10 @@ showMessage :: [(Coord,Velo)] -> [String]
 showMessage points = let  coords = map (\(c,v) -> c) points
                           ((x0,y0), _ ) = head points
                           bounds@(n,s,w,e) = foldl getBounds' (x0,x0,y0,y0) points
-                     in [ [  if (x,y) `elem` coords then '#' else '.' |   x <- [w..e] ] | y <- [n..s] ]
+                     in if (s - n < 100) && (e - w < 100) then [ [  if (x,y) `elem` coords then '#' else '.' |   x <- [w..e] ] | y <- [n..s] ]
+                                                          else [ "========" ++ show(s-n) ++ "--" ++ show(e-w) ++ "===============" ]
+                                                                        
+                                                                           
 
 nextMove :: [(Coord,Velo)] -> [(Coord, Velo)]
 nextMove points = map (\((x,y),(vx,vy)) -> ((x+vx,y+vy),(vx,vy))) points
@@ -35,5 +38,5 @@ main = do
   input <- getContents
   let constr = readConstraints (lines input)
       ((x,y),_) = head constr  in
-    putStrLn (unlines (showMessage (foldl (\pts i -> nextMove pts) constr [0..3])))
+    putStrLn (unlines (map (unlines . showMessage) (scanl (\pts i -> nextMove pts) constr [0..11000])))
   
