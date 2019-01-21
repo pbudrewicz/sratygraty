e5474185a57a4ba015f09c5913a5f4bb2b3f3a58
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-21 22:33:38 +0100

    [day201815] gnomes and elves filters

--- advent/2018/15/day201815.hs
+++ advent/2018/15/day201815.hs
@@ -4,17 +4,17 @@ import Data.List
 type Pos = (Int, Int)
 type Beast = (Pos, Char, Int)
 
-clearMap :: Char -> Char
-clearMap c | c == 'G' = '.'
+cleanMap :: Char -> Char
+cleanMap c | c == 'G' = '.'
            | c == 'E' = '.'
            | otherwise = c
 
---isInRange :: Pos -> Pos -> Bool
---isInRange (x1,y1) (x2,y2) = (abs (x1 - x2) == 1 && y1 == y2 ) || (x1 == x2 && abs (y1 - y2) == 1)
---
---moveUnits :: Int -> Array (Int,Int) Char -> [Beast] -> [Beast]
---moveUnits round field (u:us) = afterTurn:moveUnits round afterTurn us
---                              where afterTurn = (maybeAttack (maybeMove field u) u)
+isInRange :: Pos -> Pos -> Bool
+isInRange (x1,y1) (x2,y2) = (abs (x1 - x2) == 1 && y1 == y2 ) || (x1 == x2 && abs (y1 - y2) == 1)
+
+-- moveUnits :: Int -> Array (Int,Int) Char -> [Beast] -> [Beast]
+-- moveUnits round field (u:us) = afterTurn:moveUnits round afterTurn us
+--                                 where afterTurn = (maybeAttack (maybeMove field u) u)
 
 showMapElement :: Array (Int, Int) Char -> Pos  -> [Beast] -> String
 showMapElement field pos beasts | beast == [] = [field ! pos]
@@ -27,6 +27,12 @@ showBeast (pos,c,l) | l==3 = "\27[32m" ++ [c] ++ "\27[0m"
                     | l==1 = "\27[31m" ++ [c] ++ "\27[0m"
                     | otherwise = [c]
 
+elves :: [Beast] -> [Beast]
+elves  = filter (\(_,b,_) -> b == 'E') 
+
+gnomes :: [Beast] -> [Beast]
+gnomes  = filter (\(_,b,_) -> b == 'G') 
+
 beastAt :: [Beast] -> Pos -> [Beast]
 beastAt beasts pos = filter (\(bpos,_,_) -> pos == bpos) beasts
 
@@ -34,17 +40,16 @@ beastAt beasts pos = filter (\(bpos,_,_) -> pos == bpos) beasts
 -- maybeMove field unit = isInRange.... [ b | x <- rows...  blabla....
 
                                       
-
 main = do
   input <- getContents
   let rows = (length (lines input)) - 1
       cols = length (head (lines input)) - 1
       myarray = array ((0,0),(cols, rows)) [((x,y), ((lines input) !! y) !! x) | x<-[0..cols], y<-[0..rows]] 
       beasts = sort [ ((x,y),t,3)| x <-[0..cols], y<-[0..rows], t <- ['E','G'], (myarray ! (x,y)) == t ] 
-      gnomes = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'G' ] 
-      elves = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'E' ] 
-      field = array((0,0),(cols, rows)) [((x,y), (clearMap (myarray ! (x,y)))) | x<-[0..cols], y<-[0..rows] ] 
-      in putStrLn (unlines ((map show [gnomes, elves]) ++ [(show beasts)] ++ [ foldl  (\acc c -> acc ++ (showMapElement field (c,r) beasts)) "" [0..cols]  | r <- [0..rows] ] ))
+--      gnomes = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'G' ] 
+--      elves = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'E' ] 
+      field = array((0,0),(cols, rows)) [((x,y), (cleanMap (myarray ! (x,y)))) | x<-[0..cols], y<-[0..rows] ] 
+      in putStrLn (unlines ( [ foldl  (\acc c -> acc ++ (showMapElement field (c,r) beasts)) "" [0..cols]  | r <- [0..rows] ] ))
 
  --(show mytrains) ++ "\n" ++
         --(show (moveTrains tracks 0 mytrains)) ++ "\n")
