f033cf592c7e8298eee791c58b630a42ba5d20a9
Piotr Budrewicz <p.d.budrewicz@gmail.com>
2019-01-21 22:11:57 +0100

    [day 201815] coloured elves and gnomes

--- advent/2018/15/day201815.hs
+++ advent/2018/15/day201815.hs
@@ -4,20 +4,36 @@ import Data.List
 type Pos = (Int, Int)
 type Beast = (Pos, Char, Int)
 
-mapElement :: Char -> Char
-mapElement c | c == 'G' = '.'
-             | c == 'E' = '.'
-             | otherwise = c
+clearMap :: Char -> Char
+clearMap c | c == 'G' = '.'
+           | c == 'E' = '.'
+           | otherwise = c
 
-isInRange :: Pos -> Pos -> Bool
-isInRange (x1,y1) (x2,y2) = (abs (x1 - x2) == 1 && y1 == y2 ) || (x1 == x2 && abs (y1 - y2) == 1)
+--isInRange :: Pos -> Pos -> Bool
+--isInRange (x1,y1) (x2,y2) = (abs (x1 - x2) == 1 && y1 == y2 ) || (x1 == x2 && abs (y1 - y2) == 1)
+--
+--moveUnits :: Int -> Array (Int,Int) Char -> [Beast] -> [Beast]
+--moveUnits round field (u:us) = afterTurn:moveUnits round afterTurn us
+--                              where afterTurn = (maybeAttack (maybeMove field u) u)
 
-moveUnits :: Int -> Array (Int,Int) Char -> [Beast] -> [Beast]
-moveUnits round field (u:us) = afterTurn:moveUnits round afterTurn us
-                              where afterTurn = (maybeAttack (maybeMove field u) u)
+showMapElement :: Array (Int, Int) Char -> Pos  -> [Beast] -> String
+showMapElement field pos beasts | beast == [] = [field ! pos]
+                                | otherwise = showBeast (head beast)
+                                    where beast = filter (\(bpos,_,_) -> pos == bpos) beasts
 
-maybeMove :: Array (Int,Int) Char -> Beast -> [Beast]
-maybeMove field unit = isInRange.... [ b | x <- rows...  blabla.... 
+showBeast :: Beast -> String
+showBeast (pos,c,l) | l==3 = "\27[32m" ++ [c] ++ "\27[0m"
+                    | l==2 = "\27[33m" ++ [c] ++ "\27[0m"
+                    | l==1 = "\27[31m" ++ [c] ++ "\27[0m"
+                    | otherwise = [c]
+
+beastAt :: [Beast] -> Pos -> [Beast]
+beastAt beasts pos = filter (\(bpos,_,_) -> pos == bpos) beasts
+
+-- maybeMove :: Array (Int,Int) Char -> Beast -> [Beast]
+-- maybeMove field unit = isInRange.... [ b | x <- rows...  blabla....
+
+                                      
 
 main = do
   input <- getContents
@@ -27,8 +43,8 @@ main = do
       beasts = sort [ ((x,y),t,3)| x <-[0..cols], y<-[0..rows], t <- ['E','G'], (myarray ! (x,y)) == t ] 
       gnomes = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'G' ] 
       elves = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'E' ] 
-      field = array((0,0),(cols, rows)) [((x,y), (mapElement (myarray ! (x,y)))) | x<-[0..cols], y<-[0..rows] ] 
-      in putStrLn (unlines ((map show [gnomes, elves]) ++ [(show beasts)] ++ [ [field ! (c,r) | c <- [0..cols] ] | r <- [0..rows] ] ))
+      field = array((0,0),(cols, rows)) [((x,y), (clearMap (myarray ! (x,y)))) | x<-[0..cols], y<-[0..rows] ] 
+      in putStrLn (unlines ((map show [gnomes, elves]) ++ [(show beasts)] ++ [ foldl  (\acc c -> acc ++ (showMapElement field (c,r) beasts)) "" [0..cols]  | r <- [0..rows] ] ))
 
  --(show mytrains) ++ "\n" ++
         --(show (moveTrains tracks 0 mytrains)) ++ "\n")
