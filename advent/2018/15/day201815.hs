e4228c999e273e42670a8cc7cb9c919463d31f5e
p.budrewicz <p.budrewicz@samsung.local>
2019-01-24 16:37:06 +0100

    [day201815] fun with new types... (Char -> Species)

--- advent/2018/15/day201815.hs
+++ advent/2018/15/day201815.hs
@@ -3,16 +3,29 @@ import Data.List
 import qualified Data.Set as Set
 
 type Pos = (Int, Int)
-type Beast = (Pos, Char, Int)
 type Field = Array (Int, Int) Char
-data Species = Elf | Gnome deriving (Eq)
+data Species = Ghost | Elf | Gnome deriving (Eq, Ord)
+type Distance = Int
+type HitPoints = Int
+type Beast = (Pos, Species, HitPoints)
 
 instance Show Species where
+  show Ghost = " "
   show Gnome = "G"
   show Elf = "E"
 
 other :: Species -> Species
-other s = if s == Gnome then Elf else Gnome
+other s |  s == Gnome = Elf 
+        |  s == Elf = Gnome
+        |  otherwise = Ghost
+
+alive :: Species -> Bool
+alive s = s == Elf || s == Gnome
+
+specie :: Char -> Species
+specie c | c == 'G' = Gnome
+         | c == 'E' = Elf
+         | otherwise = Ghost
 
 dedup :: (Eq a) =>  [a] -> [a]
 dedup [] = []
@@ -37,21 +50,21 @@ showMapElement field pos beasts | beast == [] = [field ! pos]
                                     where beast = filter (\(bpos,_,_) -> pos == bpos) beasts
 
 showBeast :: Beast -> String
-showBeast (pos,c,l) | l == 200 = "\27[32m" ++ [c] ++ "\27[0m"
-                    | l > 150  = "\27[36m" ++ [c] ++ "\27[0m"
-                    | l > 100  = "\27[34m" ++ [c] ++ "\27[0m"
-                    | l > 50   = "\27[35m" ++ [c] ++ "\27[0m"
-                    | l > 0    = "\27[31m" ++ [c] ++ "\27[0m"
-                    | otherwise = [c]
+showBeast (pos,c,l) | l == 200 = "\27[32m" ++ show c ++ "\27[0m"
+                    | l > 150  = "\27[36m" ++ show c ++ "\27[0m"
+                    | l > 100  = "\27[34m" ++ show c ++ "\27[0m"
+                    | l > 50   = "\27[35m" ++ show c ++ "\27[0m"
+                    | l > 0    = "\27[31m" ++ show c ++ "\27[0m"
+                    | otherwise = show c
 
 elves :: [Beast] -> [Beast]
-elves  = filter (\(_,b,_) -> b == 'E') 
+elves  = filter (\(_,b,_) -> b == Elf) 
 
 gnomes :: [Beast] -> [Beast]
-gnomes  = filter (\(_,b,_) -> b == 'G') 
+gnomes  = filter (\(_,b,_) -> b == Gnome) 
 
-beastAt :: [Beast] -> Pos -> Char -- returns beast at position or empty, if no beast
-beastAt [] _ = ' '
+beastAt :: [Beast] -> Pos -> Species -- returns beast at position or empty, if no beast
+beastAt [] _ = Ghost
 beastAt ((p,b,_):bs) pos | p == pos = b
                          | otherwise = beastAt bs pos
 
@@ -61,14 +74,14 @@ beastAt ((p,b,_):bs) pos | p == pos = b
 -- scanField :: Field -> Pos -> [(Int,Pos)]
 -- scanField field pos = 
 
-adjacent = [(-1,0),(0,-1),(0,1),(1,0)] :: [(Int,Int)]
+adjacent = [(-1,0),(0,-1),(0,1),(1,0)] :: [Pos]
 
 -- chooseStep :: Field -> [Beast] -> Beast -> [Pos]
 -- chooseStep field beasts beast@((r,c),b,_) | tgts == [] = []
   --                             | otherwise = head ( [ (y+r,x+c) | (x,y) <= adjacent, 
     --                             where tgts = getTargets field (r,c) b 
 
-neighborList :: Field -> [Beast] -> [(Pos,Int,Char)] -> [(Pos,Int,Char)] -> [(Pos,Int,Char)]
+neighborList :: Field -> [Beast] -> [(Pos,Distance,Species)] -> [(Pos,Distance,Species)] -> [(Pos,Distance,Species)]
 neighborList field beasts neighbors fresh | nList == [] = newNeighbors
                                           | otherwise = neighborList field beasts newNeighbors nList
                                                       where 
@@ -76,39 +89,39 @@ neighborList field beasts neighbors fresh | nList == [] = newNeighbors
                                                         nList = dedup [((y+r,x+c),dist+1, beastAt beasts (y+r,x+c) ) | (y,x) <- adjacent,
                                                                        ((r,c),dist,_) <- fresh,
                                                                        field ! (y+r,x+c) == '.',
-                                                                       beastAt beasts (r,c) == ' ',
+                                                                       beastAt beasts (r,c) == Ghost,
                                                                        not ((y+r,x+c) `elem` (map (\(a,b,c) -> a) newNeighbors)) ] -- how to start from beast position?!
 
 
 
-showDistance :: Field -> Pos -> [Beast] -> [(Pos,Int,Char)] -> String
-showDistance field pos beasts neighbors | field ! pos == '.' && (beastAt beasts pos) == ' ' = dist
+showDistance :: Field -> Pos -> [Beast] -> [(Pos,Distance,Species)] -> String
+showDistance field pos beasts neighbors | field ! pos == '.' && (beastAt beasts pos) == Ghost = dist
                                         | otherwise = showMapElement field pos beasts
                                            where dist = getDistance pos neighbors
 
-getDistance :: Pos -> [(Pos,Int,Char)] -> String
+getDistance :: Pos -> [(Pos,Distance,Species)] -> String
 getDistance _ [] = "."
 getDistance pos ((p,dist,_):rest) | pos == p = if dist < 10 then show dist else "*"
-                                | otherwise = getDistance pos rest
+                                  | otherwise = getDistance pos rest
 
-getTargets :: Field -> [Beast] -> Pos -> Char -> [(Pos,Int,Char)]
-getTargets field beasts start beast = filter (\(_,_,b) -> b == beast) (neighborList field (filter (\(p,_,_) -> p /= start) beasts) [] [(start,0,'.')])
+getTargets :: Field -> [Beast] -> Pos -> Species -> [(Pos,Distance,Species)]
+getTargets field beasts start beast = filter (\(_,_,b) -> b == beast) (neighborList field (filter (\(p,_,_) -> p /= start) beasts) [] [(start,0,Ghost)])
 
 main = do
   input <- getContents
   let rows = (length (lines input)) - 1
       cols = length (head (lines input)) - 1
       myarray = array ((0,0),(rows, cols)) [((y,x), ((lines input) !! y) !! x) | y<-[0..rows], x <- [0..cols]] 
-      beasts = sort [ ((y,x),t,200)| y <-[0..rows], x<-[0..cols], t <- ['E','G'], (myarray ! (y,x)) == t ] 
+      beasts = sort [ ((y,x),specie(myarray ! (y,x)),200)| y <-[0..rows], x<-[0..cols], alive(specie(myarray ! (y,x)))] 
 --      gnomes = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'G' ] 
 --      elves = sort [ ((x,y),3)| x <-[0..cols], y<-[0..rows], (myarray ! (x,y)) == 'E' ] 
       field = array((0,0),(rows, cols)) [((y,x), (cleanMap (myarray ! (y,x)))) | y<-[0..rows], x<-[0..cols] ] 
-      neighbors = (neighborList field beasts [] [((9,21),0,'.')] )
-      --in putStrLn (unlines ( [ foldl  (\acc c -> acc ++ (showMapElement field (c,r) beasts)) "" [0..cols]  | r <- [0..rows] ] ))
+--      neighbors = (neighborList field beasts [] [((9,21),0,Ghost)] )
+      in putStrLn (unlines ( [ foldl  (\acc c -> acc ++ (showMapElement field (r,c) beasts)) "" [0..cols]  | r <- [0..rows] ] ))
 --      in putStrLn (unlines ( [ foldl  (\acc c -> acc ++ (showDistance field (r,c) beasts neighbors)) "" [0..cols]  | r <- [0..rows] ] ))
 --      in putStrLn (show ( getTargets field beasts (9,25) 'E'))
-      in putStrLn (show (map (\(pos,b,_) -> getTargets field beasts pos (if b == 'E' then 'G' else 'E')) beasts))
---      in putStrLn (show (neighborList field beasts [] [((20,6),0,'.')] ))
+--       in putStrLn (show (map (\(pos,b,_) -> getTargets field beasts pos (other b)) beasts))
+--      in putStrLn (show (neighborList field beasts [] [((20,6),0,Ghost)] ))
 
  --(show mytrains) ++ "\n" ++
         --(show (moveTrains tracks 0 mytrains)) ++ "\n")
